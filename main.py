68367d3f2b95720b44a50dd84a8b38b8f41569ce
shayan <shayanbali@gmail.com>
2021-09-20 15:48:59 +0430

    Add split data section

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ except:
       """)
     quit()
 
+# Preprocess data
 df = preprocess(df)
+
 # Scale the features
 df_prescaled = df.copy()
 df_scaled = df.drop(['amount'], axis=1)
@@ -31,6 +33,10 @@ df_scaled = pd.DataFrame(df_scaled, columns=cols, index=df.index)
 df_scaled = pd.concat([df_scaled, df['amount']], axis=1)
 df = df_scaled.copy()
 
+# Split the dataframe into a training and testing set
+X = df.loc[:, df.columns != 'amount']
+y = df.amount
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 print(df.loc[df['week'] == 'Friday', ['week']])
 print(df.head())

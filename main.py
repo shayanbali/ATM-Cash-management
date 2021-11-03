3f8b91e92d70776c564ec97ec2fe086cc95f959e
shayan <shayanbali@gmail.com>
2021-11-03 09:42:49 +0330

    New structure

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ from keras.layers import Dense, Dropout
 from keras.layers import BatchNormalization
 from sklearn.metrics import mean_squared_error
 
+
+# Getting Data set
 try:
+    a = input("please enter file:")
     print("Reading in the dataset. This will take some time...")
-    df = pd.read_csv('data_predictor2.csv', nrows=720)
+    df = pd.read_csv(a, nrows=720)
 except:
     print("""
       can not import dataset
@@ -27,9 +30,9 @@ df_copy = df.copy()
 df = preprocess(df)
 
 print(df.loc[df['week'] == 'Friday', ['week']])
-print(df.head() )
-print("sdssdsd")
-# Scale the features
+print(df.head())
+
+# Scales the features
 df_prescaled = df.copy()
 df_scaled = df.drop(['amount'], axis=1)
 df_scaled = scale(df_scaled)
@@ -39,40 +42,29 @@ df_scaled = pd.DataFrame(df_scaled, columns=cols, index=df.index)
 df_scaled = pd.concat([df_scaled, df['amount']], axis=1)
 df = df_scaled.copy()
 
-
 # Split the dataframe into a training and testing set
 X = df.loc[:, df.columns != 'amount']
 y = df.amount
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Build neural network in Keras
+# Builds neural network in Keras
 model = Sequential()
-#model.add(Dropout(0.2, input_shape=(X_train.shape[1],)))
-#model.add(BatchNormalization(input_shape=(X_train.shape[1],)))
 model.add(Dense(1000, activation='relu'
-               , input_dim=X_train.shape[1]))
+                , input_dim=X_train.shape[1]))
 model.add(BatchNormalization())
 model.add(Dense(800, activation='relu'))
 model.add(Dense(400, activation='relu'))
-#model.add(Dropout(0.2))
-#model.add(Dense(50, activation='relu'))
-#model.add(Dropout(0.2))
-#model.add(Dense(8, activation='relu'))
-#model.add(Dropout(0.2))
 model.add(Dense(1))
 
-
-
-#32
 # Compile model
 model.compile(loss='mse', optimizer='adam', metrics=['mse'])
 
 model.fit(X_train, y_train, epochs=5050)
 print(df.head())
 print(X_train.shape[1])
+
 # Results
 train_pred = model.predict(X_train)
-
 print("------------------------------")
 train_rmse = np.sqrt(mean_squared_error(y_train, train_pred))
 test_pred = model.predict(X_test)
@@ -82,27 +74,25 @@ print("Test RMSE: {:0.2f}".format(test_rmse))
 print('------------------------')
 
 
+# predict a random day in our data set
 def predict_random(df_prescaled, X_test, model):
     sample = X_test.sample(n=1, random_state=np.random.randint(low=0, high=50))
     idx = sample.index[0]
 
-    actual_fare = df_prescaled.loc[idx,'amount']
+    actual_fare = df_prescaled.loc[idx, 'amount']
     day_names = ['Sunday', 'monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     day_of_week = df_copy.loc[idx, 'week']
     year = df_prescaled.loc[idx, 'year']
     month = df_prescaled.loc[idx, 'month']
     day = df_prescaled.loc[idx, 'day']
     predicted_fare = model.predict(sample)[0][0]
-    rmse = np.sqrt(np.square(predicted_fare-actual_fare))
+    rmse = np.sqrt(np.square(predicted_fare - actual_fare))
 
     print("withdraw Details: {}, {}-{}-{}".format(day_of_week, year, month, day))
     print("Actual fare: ${:0.2f}".format(actual_fare))
     print("Predicted fare: ${:0.2f}".format(predicted_fare))
     print("RMSE: ${:0.2f}".format(rmse))
     print("Train average: ", df_prescaled['amount'].mean())
-    # print("Test average: ", y_test.mean())
+
 
 predict_random(df_prescaled, X_test, model)
-
-
-
--- utils.py
+++ utils.py
@@ -1,21 +1,24 @@
 def preprocess(df):
-    # remove missing values in the dataframe
+
+    # Removes missing values in the dataframe
     def remove_missing_values(df):
         df = df.dropna()
 
         df = df[(df['amount'] > 0)]
         for ind in df.index:
             if df['amount'][ind] == 0:
+
                 df.drop(ind)
 
         return df
 
-    # remove outliers in fare amount
+    # Removes outliers in fare amount
     def remove_amount_outliers(df, lower_bound, upper_bound):
+
         df = df[(df['amount'] > lower_bound) & (df['amount'] <= upper_bound)]
         return df
 
-    # change friday with  2
+    # Changes friday with  2
     def change_friday(df):
         for ind in df.index:
             if df['week'][ind] == 'Friday':
@@ -23,12 +26,17 @@ def preprocess(df):
 
         return df
 
+    # Adds one to holiday column
     def add_one_holiday(df):
         for ind in df.index:
+
             df['holiday'][ind] += 1
+
         return df
 
+    # Converts day to numeric value
     def day_to_num(df):
+
         df['week'] = df['week'].replace('Saturday', 1)
         df['week'] = df['week'].replace('Sunday', 2)
         df['week'] = df['week'].replace('Monday', 3)
@@ -39,15 +47,21 @@ def preprocess(df):
 
         return df
 
+    # Adds the average of the 7 previous days
     def add_avg(df):
         for ind in df.index:
             if ind - 7 < 0:
+
                 h = df.iloc[0:ind + 1, 0].mean()
+
             else:
+
                 h = df.iloc[ind - 7:ind + 1, 0].mean()
+
             df['avg'][ind] = h
         return df
 
+    # Adds season column
     def add_season(df):
         for ind in df.index:
             if 0 < df['month'][ind] < 4:
@@ -61,6 +75,7 @@ def preprocess(df):
 
         return df
 
+    # Recognizes whether the day before is a holiday or not
     def add_before(df):
         for ind in df.index:
             try:
@@ -71,16 +86,25 @@ def preprocess(df):
 
         return df
 
+    # Recognizes whether we are in the first days of the month or not
+    def add_beginnig(df):
+        for ind in df.index:
+            if 0 < df['day'][ind] < 8:
+                df['beginning'][ind] = 1
+
+        return df
+
     print(df.shape, "     shape")
+
+    # Running functions
     df = add_avg(df)
     df = add_season(df)
     df = remove_missing_values(df)
-    print(df.shape,"     shapeeeee")
     df = remove_amount_outliers(df, lower_bound=40000000, upper_bound=150000000)
-    print(df.shape, "     shapee")
     df = change_friday(df)
     df = day_to_num(df)
     df = add_one_holiday(df)
     df = add_before(df)
+    df = add_beginnig(df)
     print(df.shape)
     return df
